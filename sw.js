25e258d000ecfac44e67f3de0986829a63d8e6ec
Bry Onyoni <bryonyoni@gmail.com>
2026-04-25 22:47:57 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
-const version = '7.44(beta)';
+const version = '7.46(beta)';
 self.addEventListener('push', event => {
   const data = event.data.json();
   const get_language = () => {
